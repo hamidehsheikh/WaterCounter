a5da98b30298ed89a2cc79fba1673f474c3d2776
hamidehsheikh <hamidehsheikh@gmail.com>
2022-06-04 19:27:02 +0430

    Initial Commit

---
+++ WaterCounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterCounter
+//
+//  Created by Hamideh Sheikh on 6/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaterCounter/WaterCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterCounterApp.swift
+//  WaterCounter
+//
+//  Created by Hamideh Sheikh on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WaterCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
